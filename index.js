9bb5116579e45588c6d2824c070bf85cf5abbbd9
klevchuk <lemoniklava@gmail.com>
2023-09-08 14:19:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let n = 3;
+let s = "gfg"
+function repeatStr (n, s) {
+    if (n % 1 === 0) {
+      console.log(n * s);
+      }
+  }
+  repeatStr (n, "");
+
+
+  function abbrevName(name){
+
+  console.log(name.toUpperCase(0));
+
+}
+
+abbrevName("afd oip");
