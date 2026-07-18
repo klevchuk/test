468263eafb2a7b37e772ebd525bb5727a5313d37
klevchuk <86412131+klevchuk@users.noreply.github.com>
2026-07-18 16:59:28 +0800

    Create schedule.js

---
+++ schedule.js
@@ -0,0 +1,130 @@
+const schedule = {
+    "18 июля": {
+        "Day off": [
+            "Denis",
+            "Anya",
+            "Lada"
+        ],
+        "❗️11:00 Chimelong hotel Машинка в 10:30 с общаги  ❗️обувь взять с собой": [
+            "Ilya",
+            "Vlad",
+            "Datri",
+            "Olha",
+            "Milka"
+        ],
+        "❗️12:40 Samba (come at 12:35)": [
+            "Avrora",
+            "Vika",
+            "Alina"
+        ],
+        "❗️13:00 You can do it (come at 12:30)": [
+            "Yaroslav",
+            "Yulian",
+            "Dima",
+            "Max",
+            "Baluta",
+            "Olga",
+            "Milka",
+            "Di"
+        ],
+        "❗️14:00 Cheerleading (детская, come at 13:55)": [
+            "Mery",
+            "Milka",
+            "Diana",
+            "Marina",
+            "Olha"
+        ],
+        "❗️14:10 Samba (come at 14:05)": [
+            "Avrora",
+            "Vika",
+            "Alina"
+        ],
+        "❗️ 14:20 мини парад (come at 14:15)": [
+            "Alexandra"
+        ],
+        "❗️15:30 Parade (come at 15:00) 15:15 собрание ⚠️ 🍉Арбуз": [
+            "Yulian",
+            "Yaroslav",
+            "Ilya",
+            "Mery",
+            "Datri",
+            "Diana"
+        ],
+        "❗️15:30 Parade (come at 15:00) 15:15 собрание ⚠️  🎊Samba": [
+            "Lemon",
+            "Dima",
+            "Alexey",
+            "Avrora",
+            "Vika",
+            "Alina"
+        ],
+        "❗️15:30 Parade (come at 15:00) 15:15 собрание ⚠️ 💃нпс": [
+            "Miya",
+            "Alexandra",
+            "Katya",
+            "Di"
+        ],
+        "❗️15:30 Parade (come at 15:00) 15:15 собрание ⚠️ 🦏Носорог": [
+            "Baluta",
+            "Marina"
+        ],
+        "❗️15:30 Parade (come at 15:00) 15:15 собрание ⚠️ 🦬Бык": [
+            "Olha"
+        ],
+        "❗️15:30 Parade (come at 15:00) 15:15 собрание ⚠️ 🦂Scorpion": [
+            "Vera"
+        ],
+        "❗️16:45 Cheerleading (детская, come at 16:40)": [
+            "Datri",
+            "Mery",
+            "Miya",
+            "Marina",
+            "Di"
+        ],
+        "❗️ 17:00 мини парад (come at 16:55)": [
+            "Vera"
+        ],
+        "❗️17:00 You can do it (come at 16:45)": [
+            "Yaroslav",
+            "Alexey",
+            "Lemon",
+            "Max",
+            "Baluta",
+            "Milka",
+            "Alexandra",
+            "Katya"
+        ],
+        "❗️17:40 Tusa  (come at 17:35)": [
+            "Olha"
+        ],
+        "❗️18:20 Cheerleading (детская, come at 18:10)": [
+            "Baluta",
+            "Katya",
+            "Miya",
+            "Datri",
+            "Di"
+        ],
+        "❗️18:55 Samba (come at 18:50)": [
+            "Avrora",
+            "Vika",
+            "Alina"
+        ],
+        "❗️19:15 Party (come at 18:45)": [
+            "Lemon",
+            "Ilya",
+            "Yulian",
+            "Alexey",
+            "Max",
+            "Dima",
+            "Mery",
+            "Miya",
+            "Alexandra",
+            "Katya",
+            "Marina",
+            "Diana"
+        ],
+        "❗️20:00 репетиция в цирковом": [
+            "Di", "Alexandra" ,"Alina", "Yulian", "Denis", "Alexey"
+        ]
+    }
+};
